ede5e714dab034f1d8a9b5f4682e3f7cb672d52e
emetras- | elmer metras <emetras-@student.42sp.org.br>
2022-05-29 03:20:13 +0200

    ex20 fixed

--- Reloaded/ex20/ft_strdup.c
+++ Reloaded/ex20/ft_strdup.c
@@ -6,11 +6,24 @@
 /*   By: emetras- <emetras-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 17:43:01 by emetras-          #+#    #+#             */
-/*   Updated: 2022/05/27 16:03:10 by emetras-         ###   ########.fr       */
+/*   Updated: 2022/05/29 03:19:45 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
 
 char	*ft_strdup(char *src)
 {
@@ -18,14 +31,15 @@ char	*ft_strdup(char *src)
 	int		i;
 
 	i = 0;
-	if (sizeof(src) < 1)
+	p = malloc(ft_strlen(src));
+	if (!p)
 		return (NULL);
-	p = malloc(sizeof(src));
 	while (src[i])
 	{
 		p[i] = src[i];
 		i++;
 	}
+	printf("%d", i);
 	p[i] = '\0';
 	return (p);
 }
